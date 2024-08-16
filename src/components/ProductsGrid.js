b51b3434a5b4f3c1f4fd7f8f2c7c038fa03c9743
Preeti <pyadav2007@gmail.com>
2024-08-16 12:26:36 -0400

    added filter

--- src/components/ProductsGrid.js
+++ src/components/ProductsGrid.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "../styles.css";
-import Product from "./product";
+import Product from "./Product.js";
 
 export default function ProductGrid() {
   const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [price, setprice] = useState("");
+  const [rating, setRating] = useState("");
 
   useEffect(() => {
     fetch("products.json")
@@ -14,11 +17,90 @@ export default function ProductGrid() {
       });
   }, []);
 
+  const handleSearch = (e) => setSearch(e.target.value);
+  const priceHandler = (e) => setprice(e.target.value);
+  const ratingHandler = (e) => setRating(e.target.value);
+
+  const matchesPrice = (productPrice, price) => {
+    if (price === "<=$10") {
+      return productPrice <= 10;
+    } else if (price === "$10-$20") {
+      return productPrice > 10 && productPrice <= 20;
+    } else if (price === "$20-$30") {
+      return productPrice > 20 && productPrice <= 30;
+    } else if (price === "$30-$40") {
+      return productPrice > 30 && productPrice <= 40;
+    } else if (price === "$40-$50") {
+      return productPrice > 40 && productPrice <= 50;
+    }
+    return true;
+  };
+
+  const filterProducts = products.filter((product) => {
+    const matchesSearch = product.title
+      .toLowerCase()
+      .includes(search.toLowerCase());
+    const productPrice = product.price;
+    const matchesRate = !rating || product.rating === parseFloat(rating);
+    return matchesPrice(productPrice, price) && matchesRate && matchesSearch;
+  });
+
   return (
-    <div className="product">
-      {products.map((product) => (
-        <Product produc={product}></Product>
-      ))}
+    <div>
+      {/* Creating Search Bar */}
+      <input
+        className="search-input"
+        type="text"
+        placeholder="search product..."
+        value={search}
+        onChange={handleSearch}
+      />
+      {/* Creating Price and Rating filter */}
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Price</label>
+          <select
+            className="filter-dropdown"
+            onChange={priceHandler}
+            value={price}
+          >
+            <option value="">All Prices</option>
+            <option value="<=$10">{"<=$10"}</option>
+            <option value="$10-$20">{"$10-$20"}</option>
+            <option value="$20-$30">{"$20-$30"}</option>
+            <option value="$30-$40">{"$30-$40"}</option>
+            <option value="$40-$50">{"$40-$50"}</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={ratingHandler}
+          >
+            <option>Rating</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+          </select>
+        </div>
+      </div>
+
+      {/* Creating Main Page  */}
+      <div className="products-grid">
+        {filterProducts.map((product) => (
+          /* Calling product component */
+          <Product product={product} key={product.id}></Product>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/product.js
+++ src/components/product.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-export default function Product(product) {
+export default function Product({ product }) {
+  const errorHandler = (e) => (e.target.src = "images/default.jfif");
+
+  const getRating = (rating) => {
+    if (rating >= 8) return "rating-good";
+
+    if (rating >= 5 && rating < 8) return "rating-ok";
+    else return "rating-bad";
+  };
+
   return (
-    <div key={product.id}>
-      <img src={`images/${product.image}`} alt={product.title} />
+    <div key={product.id} className="product">
+      <img
+        src={`images/${product.image}`}
+        alt={product.title}
+        onError={errorHandler}
+      />
       <div className="product-info">
         <h3 className="product-title">{product.title}</h3>
-        <p className="product-price">{product.price}</p>
-        <p className="product-rating">{product.rating}</p>
+        <p className="product-price">${product.price}</p>
+        <p className={`product-rating ${getRating(product.rating)}`}>
+          {product.rating}
+        </p>
         <p className="product-description">{product.description}</p>
       </div>
     </div>
