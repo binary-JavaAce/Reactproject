d7acc281106e98072a0fe6068cc312509c343fdb
Preeti <pyadav2007@gmail.com>
2024-08-22 20:54:48 -0400

    reset branch

--- src/services/axiosInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Base Axios instance without JWT token
-export const url = axios.create({
-  baseURL: "http://localhost:8080",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
